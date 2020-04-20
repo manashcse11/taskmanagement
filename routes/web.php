1c2b24ed188e2141a7ed37640ba90435fad845cd
Manash <manash@cefalo.com>
2020-04-20 18:08:49 +0600

    Setup basic layout for starting

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TaskController@index')->name('home');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
